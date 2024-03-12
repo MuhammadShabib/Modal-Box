4435dd6ede49aa1412f6de00a4307b0b1f2095fa
shabib <shabib@gmail.com>
2024-03-12 16:36:23 +0430

    adding some func

--- main.js
+++ main.js
@@ -5,13 +5,25 @@ const div = document.querySelector("div")
 
 open1.addEventListener("click", () => {
     div.style.display = "block"
+    if (div.style.display == "block") {
+        document.querySelector("body").style.backgroundColor = "red"
+        console.log("salm")
+    }
 })
 close1.addEventListener("click", () => {
     div.style.display = "none"
+    if (div.style.display == "none") {
+        document.querySelector("body").style.backgroundColor = "aqua"
+        console.log("salm")
+    }
 })
 window.addEventListener("click", (event) => {
     if (event.target == document.querySelector("body")) {
         div.style.display = "none"
+    }
+    if (div.style.display == "none") {
+        document.querySelector("body").style.backgroundColor = "aqua"
+        console.log("salm")
     }
 
 })
