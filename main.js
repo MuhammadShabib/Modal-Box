0d30143b3646f39f98221af9f123bb058d9673c6
shabib <shabib@gmail.com>
2024-03-12 18:27:02 +0430

    Adding some settings

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
 const open1 = document.getElementById("open")
 const close1 = document.getElementById("close")
-const div = document.querySelector("div")
-
-
+const container = document.getElementById("modal-container")
+const content = document.querySelector("#modal-content")
 open1.addEventListener("click", () => {
-    div.style.display = "block"
-    if (div.style.display == "block") {
-        document.querySelector("body").style.backgroundColor = "red"
-        console.log("salm")
-    }
+    container.style.display = "block"
+    content.style.display = "block"
 })
 close1.addEventListener("click", () => {
-    div.style.display = "none"
-    if (div.style.display == "none") {
-        document.querySelector("body").style.backgroundColor = "aqua"
-        console.log("salm")
-    }
+    container.style.display = "none"
+    content.style.display = "block"
 })
 window.addEventListener("click", (event) => {
-    if (event.target == document.querySelector("body")) {
-        div.style.display = "none"
+    if (event.target == container) {
+        container.style.display = "none"
     }
-    if (div.style.display == "none") {
-        document.querySelector("body").style.backgroundColor = "aqua"
-        console.log("salm")
-    }
-
 })
